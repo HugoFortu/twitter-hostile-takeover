c179364bfa2905440ae389ebbcefe77032c9b241
hugo fortunier <hfortunier@gmail.com>
2024-02-21 20:49:49 +0100

    Bugfix: fix nuxt refresh

--- frontend/pages/Login.vue
+++ frontend/pages/Login.vue
@@ -1,32 +1,30 @@
 <template>
   <v-form v-model="valid">
       <v-container style="margin-top: 100px;">
-        <v-row>
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="email"
-              label="Email"
-              hide-details
-              required
-            ></v-text-field>
-          </v-col>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="email"
+            label="Email"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
 
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="password"
-              :counter="10"
-              label="Password"
-              hide-details
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="password"
+            :counter="10"
+            label="Password"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
       </v-container>
     </v-form>
 </template>
