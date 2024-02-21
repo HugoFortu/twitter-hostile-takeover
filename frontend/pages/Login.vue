872e522e8ee3b460bb298ab91e48b24edb7a0325
hugo fortunier <hfortunier@gmail.com>
2024-02-21 17:03:54 +0100

    Modify Docker config to fix build error

--- frontend/pages/Login.vue
+++ frontend/pages/Login.vue
@@ -1,6 +1,37 @@
 <template>
-  
+  <v-form v-model="valid">
+      <v-container style="margin-top: 100px;">
+        <v-row>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="email"
+              label="Email"
+              hide-details
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="password"
+              :counter="10"
+              label="Password"
+              hide-details
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
 </template>
 <script setup>
-
+const valid = null
+const email = ""
+const password = ""
 </script>
