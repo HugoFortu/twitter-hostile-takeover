afc8294dc39131d92e0c6b37c7d6bcac26c42805
hugo fortunier <hfortunier@gmail.com>
2024-02-21 21:28:02 +0100

    Create tweet & comment models

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
